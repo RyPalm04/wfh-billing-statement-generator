cd0eff4f83125f28d1c3d3cc4803c8aea5776bb5
Ryan Palmer <nayrremlap@gmail.com>
2022-02-22 11:27:20 -0600

    Add basic TabFour

--- src/main/java/com/palmer/billingstatementgenerator/views/tabs/GeneratorTabs.java
+++ src/main/java/com/palmer/billingstatementgenerator/views/tabs/GeneratorTabs.java
@@ -1,22 +1,71 @@
 package com.palmer.billingstatementgenerator.views.tabs;
 
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.GridPane;
 
+import java.text.NumberFormat;
+
 public abstract class GeneratorTabs extends Tab {
+    protected static final NumberFormat DOLLAR_FORMATTER = NumberFormat.getCurrencyInstance();
+    private final boolean showPrev;
+    private final boolean showNext;
+    private final boolean showClear;
     protected Button nextButton = new Button("Next");
     protected Button prevButton = new Button("Previous");
-    protected GridPane root;
+    protected Button clearButton = new Button("Clear Selections");
+    protected GridPane grid = new GridPane();
+    protected GridPane root = new GridPane();;
 
-    public GeneratorTabs() {
+    public GeneratorTabs(String tabTitle) {
+        this(tabTitle, true, true, true);
+    }
+    public GeneratorTabs(String tabTitle, boolean showPrev, boolean showNext, boolean showClear) {
+        super(tabTitle);
+        this.showPrev = showPrev;
+        this.showNext = showNext;
+        this.showClear = showClear;
         createAndConfigurePanel();
+        configureGrid();
         addForm();
+        clearButton.setDisable(true);
     }
 
-    protected abstract void addForm();
+    protected void createAndConfigurePanel() {
+        root.setAlignment(Pos.CENTER);
+        root.setHgap(7);
+        root.setVgap(10);
 
-    protected abstract void createAndConfigurePanel();
+        GridPane.setConstraints(grid, 1, 0);
+        GridPane.setConstraints(prevButton, 0, 1);
+        GridPane.setConstraints(nextButton, 2, 1);
+        GridPane.setConstraints(clearButton, 1, 1);
+        GridPane.setHalignment(clearButton, HPos.CENTER);
+
+        root.getChildren().add(grid);
+
+        if (showPrev) {
+            root.getChildren().add(prevButton);
+        }
+
+        if (showNext) {
+            root.getChildren().add(nextButton);
+        }
+
+        if (showClear) {
+            root.getChildren().add(clearButton);
+        }
+
+        this.setContent(root);
+    }
+
+    protected void addGridElements(Node... nodes) {
+        grid.getChildren().addAll(nodes);
+    }
 
     public Button getNextButton() {
         return nextButton;
@@ -25,4 +74,12 @@ public abstract class GeneratorTabs extends Tab {
     public Button getPrevButton() {
         return prevButton;
     }
+
+    private void configureGrid() {
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(7);
+        grid.setVgap(10);
+    }
+
+    protected abstract void addForm();
 }
